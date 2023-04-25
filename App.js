a92a61c604ad6cebff3a76ab30cbdff7a7903380
Soin Roman <soinroma26@gmail.com>
2023-04-26 00:00:09 +0500

    - updated files

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Mama Privet</Text>
+      <Text style={styles.text}>React Native</Text>
       <StatusBar style="auto" />
     </View>
   );т
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # react-native-base
-Базовая сборка React Native
+Базовая сборка React Native через Expo-Cli
+
+Сначала вам нужно установить глобально expo-cli чтобы были доступны его команды
+npm i -g expo-cli
+
+
+
